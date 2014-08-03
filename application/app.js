ba62f3dee4064b90ed2dc3f123c42b28489ad42e
Tristan <thrawst88@gmail.com>
2014-08-03 14:40:14 -0600

    updated to express 3.x

--- application/app.js
+++ application/app.js
@@ -1,78 +1,46 @@
-
-var http = require('http').createServer(start).listen(8000);
-var mime = require('mime');
-var url  = require('url');
-var qs 	 = require('querystring');
-var jade = require('jade');
-
+var express = require('express');
 var numberDictionary = require('./numberDictionary');
 
 var langCount = numberDictionary.numberOfSupportedLanguages();
 var languages = numberDictionary.supportedLanguages();
 
-//Web server startup.
-function start(request, response) {
-	var isCurl = false;
-	if (typeof(request.headers['user-agent']) == 'string' && 
-		request.headers['user-agent'].length > 4){
-		isCurl = request.headers['user-agent'].substring(0,4).toLowerCase() == 'curl';
-	}
-	var page = parsePage(url.parse(request.url).pathname, isCurl);
-	
-	if (!isCurl){
-		var responsePage = jade.renderFile(__dirname+'/../views' + page[0], page[1], function (err, html) {
-			if (err){
-		  		response.writeHead(500);
-		  		console.log(err);
-				return response.end('Error loading page');
-	        }
-			response.writeHead(200, {'Content-Type': 'text/html'});
-			response.write(html);
-			response.end();
-		});
-	}
-	else{
-		response.writeHead(200, {'Content-Type': 'application/json'});
-		response.write(JSON.stringify(page));
-		response.end();
-	}
+// Configuration
+var app = express();
 
-}
+app.configure( function() {
+    app.set('view engine', 'jade');
+    app.set('views', __dirname + '/../views');
+});
 
-function parsePage(page, isCurl){
-	//console.log(page);
-	var newPage = ['',{}];
-	var regexp = /(\/\w*\/\d*$)|(\/\w*\/-\d*$)/g; // /word/number or /word/-number
+app.get('/', function(req, res){
+    res.render('index.jade', {'languageCount': langCount, 'languages': languages});
+});
 
-	if (!isCurl){
-		if (page == '' || page == '/'){
-			newPage = ['/index.jade', {'languageCount': langCount, 'languages': languages}];
-		}
-		else if (regexp.test(page)){
-			newPage[0] = '/number.jade';
-			var dict = {};
-			dict.number = getNumber(page);
-			dict.language = getLanguage(page);
-			dict.parsedNumber = getNumberInLanguage(dict.number, dict.language);
-			newPage[1] = dict;
-		}
-	}
-	else{		
-		if (regexp.test(page)){
-			var dict = {};
-			dict.number = getNumber(page);
-			dict.language = getLanguage(page);
-			dict.parsedNumber = getNumberInLanguage(dict.number, dict.language);
-			if (dict.number != 'error' && dict.language != 'error'){
-				newPage = dict;
-			}
-		}
-		else{
-			newPage = {'error': 'usage: `curl [address]/[language]/[number]`'};
-		}
-	}
-	return newPage;
-}
+app.get('/:language/:number', function(req, res){
+    var language = req.params.language;
+    var number = parseInt(req.params.number);
+    res.render('number.jade', {
+        'language':req.params.language, 
+        'number':req.params.number, 
+        'parsedNumber': getNumberInLanguage(number, language)
+    });
+});
+
+app.post('/', function(req, res){
+    res.send({
+        'error': 'usage: `curl [address]/[language]/[number]`'
+    }); 
+});
+
+app.post('/:language/:number', function(req, res){
+    var language = req.params.language;
+    var number = parseInt(req.params.number);
+    res.send({
+        'language':req.params.language, 
+        'number':req.params.number, 
+        'parsedNumber': getNumberInLanguage(number, language)
+    });
+});
 
 function getNumber(line){
 	var len = line.length;
@@ -88,24 +56,8 @@ function getNumber(line){
 	return 'error';
 }
 
-function getLanguage(line){
-	var len = line.length;
-	var ret = '';
-
-	for (var i=1; i<len; i++){
-		if (line[i] == '/'){
-			return ret;
-		}
-		else if ( new RegExp(/[^\w]/g).test(line[i])){
-			break;
-		}
-		else{
-			ret += line[i];
-		}
-	}
-	return 'error';
-}
-
 function getNumberInLanguage(number, language){
 	return numberDictionary.parseNumberForLanguage(number,language);
 }
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 //index.js
 
 var app = require('./application/app');
+
+app.listen(process.env.PORT ? process.env.PORT : 8000 , function() {
+    console.log('Listening...');
+});
