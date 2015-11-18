5c31b17ad60f791bf6ec74bed2a92cc96f1bca67
Wagner Leonardi <leonardiwagner@gmail.com>
2015-11-18 20:27:34 -0200

    code style fix

--- application/parsers/portugueseNumberParser.js
+++ application/parsers/portugueseNumberParser.js
@@ -12,8 +12,8 @@ var portugueseNumberParser = function(){
     6:'seis', 7:'sete', 8:'oito', 9:'nove', 10:'dez',
     11:'onze', 12:'doze', 13:'treze', 14:'quatorze', 15: 'quinze',  16: 'dezesseis', 17: 'dezessete', 18: 'dezoito', 19: 'dezenove',
     20:'vinte',
-    30:'trinta', 40:'quarenta', 50:'cinquenta', 60:'sesenta', 70:'setenta', 80:'oitenta', 90:'noventa', 100:'cem',
-    '100s': 'cento', '200': 'duzentos', '300': 'trezentos', '400': 'quatrocentos', '500': 'quinhentos',
+    30:'trinta', 40:'quarenta', 50:'cinquenta', 60:'sesenta', 70:'setenta', 80:'oitenta', 90:'noventa',
+    100:'cem', '100s': 'cento', 200: 'duzentos', 300: 'trezentos', 400: 'quatrocentos', 500: 'quinhentos',
     600: 'seiscentos', 700: 'setecentos', 800: 'oitocentos', 900: 'novecentos',
     1000: 'mil', 1000000:'milhão', 1000000000:'bilhão', 1000000000000:'trilhão'};
 
